b200be4e50e54498fbb928c4abb450efc2cf7959
CodieGlot <ngnhathuy1224@gmail.com>
2024-02-21 14:02:15 +0700

    update

--- src/java/Services/AuthService.java
+++ src/java/Services/AuthService.java
@@ -1,11 +1,32 @@
 package Services;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.impl.crypto.MacProvider;
+
+import javax.crypto.SecretKey;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.TemporalAmount;
+import java.util.Date;
+
 import Domain.DTOs.AuthDto.LoginPayloadDto;
 import Domain.Exceptions.NotFoundException;
 import Domain.Exceptions.UnauthorizedException;
 import Domain.Models.User;
 
 public class AuthService {
+    private final String CLAIM_ROLE = "role";
+    private final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
+    private final SecretKey SECRET_KEY = MacProvider.generateKey(SIGNATURE_ALGORITHM);
+    private final TemporalAmount TOKEN_VALIDITY = Duration.ofHours(4L);
+    
     private UserService userService;
 
     public AuthService() {
@@ -22,7 +43,41 @@ public class AuthService {
         if (!user.getPassword().equals(password)) {
             throw new UnauthorizedException("Username or Password incorrect");
         }
+        
+        String accessToken = createToken(user.getUsername(), user.getUserRole().toString());
 
-        return new LoginPayloadDto(user, "");
+        return new LoginPayloadDto(user, accessToken);
+    }
+
+    /**
+     * Builds a JWT with the given subject and role and returns it as a JWS signed
+     * compact String.
+     */
+    public String createToken(String subject, String role) {
+        final Instant now = Instant.now();
+        final Date expiryDate = Date.from(now.plus(TOKEN_VALIDITY));
+        return Jwts.builder()
+                .setSubject(subject)
+                .claim(CLAIM_ROLE, role)
+                .setExpiration(expiryDate)
+                .setIssuedAt(Date.from(now))
+                .signWith(SIGNATURE_ALGORITHM, SECRET_KEY)
+                .compact();
+    }
+
+    /**
+     * Parses the given JWS signed compact JWT, returning the claims.
+     * If this method returns without throwing an exception, the token can be
+     * trusted.
+     */
+    public Jws<Claims> parseToken(String compactToken)
+            throws ExpiredJwtException,
+            UnsupportedJwtException,
+            MalformedJwtException,
+            SignatureException,
+            IllegalArgumentException {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(compactToken);
     }
 }
